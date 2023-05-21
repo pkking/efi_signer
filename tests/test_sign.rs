28718af230a5a452ed00cdf4762fcf3ca02f40bc
Li Chaoran <pkwarcraft@gmail.com>
2023-05-21 17:41:43 +0800

    add some tests
    Signed-off-by: Li Chaoran <pkwarcraft@gmail.com>

--- tests/test_sign.rs
+++ tests/test_sign.rs
@@ -44,3 +44,43 @@ fn test_sign() {
         new_pe.compute_check_sum().unwrap()
     )
 }
+
+#[test]
+fn test_sign_append() {
+    init();
+    let buf = include_bytes!("./shimx64.efi.signed");
+    let pe = efi_signer::EfiImage::parse(buf).unwrap();
+
+    let sig = pe
+        .sign_signature(
+            PathBuf::from_str("./tests/certificate.p7b").unwrap(),
+            PathBuf::from_str("./tests/key.pem").unwrap(),
+            None,
+            efi_signer::DigestAlgorithm::Sha256,
+        )
+        .unwrap();
+
+    let new_pe = efi_signer::EfiImage::parse(&sig).unwrap();
+    assert_eq!(
+        new_pe.get_digest_algo().unwrap().unwrap(),
+        efi_signer::DigestAlgorithm::Sha256
+    );
+    assert_eq!(
+        pe.compute_digest(efi_signer::DigestAlgorithm::Sha256)
+            .unwrap(),
+        new_pe
+            .compute_digest(efi_signer::DigestAlgorithm::Sha256)
+            .unwrap()
+    );
+    assert_eq!(
+        new_pe
+            .compute_digest(efi_signer::DigestAlgorithm::Sha256)
+            .unwrap(),
+        new_pe.get_digest().unwrap().unwrap()
+    );
+    assert_eq!(new_pe.signatures.len(), 2);
+    assert_eq!(
+        new_pe.get_checksum_from_header().unwrap(),
+        new_pe.compute_check_sum().unwrap()
+    )
+}
