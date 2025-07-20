4a42ffd40388a680c7ee00568556626cee4d35a4
lichaoran <pkwarcraft@gmail.com>
2025-07-20 10:02:36 +0800

    fix fmt
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- tests/test_parse.rs
+++ tests/test_parse.rs
@@ -157,10 +157,9 @@ fn test_verify_invalid_cert() {
     let paths = vec!["./tests/key.pem".to_string()];
     match pe.verify(paths) {
         Ok(_) => panic!("we should failed"),
-        Err(e) => assert!(
-            e.to_string()
-                .contains(&"Failed to decode a pem cert into Cert struct".to_string())
-        ),
+        Err(e) => assert!(e
+            .to_string()
+            .contains(&"Failed to decode a pem cert into Cert struct".to_string())),
     }
 }
 
